53e8cb294cab0cc2f545cca496640d1716d53d01
pawajee <pawajee@users.noreply.github.com>
2024-07-21 17:38:36 +0300

    Update FamilyMagNav.js

--- src/JavaScript/FamilyMagNav.js
+++ src/JavaScript/FamilyMagNav.js
@@ -1,53 +1,70 @@
-$(document).unbind('keydown').keydown(function(e){
-	console.log('keydown');
-	if(!!e.keyCode){
+var loadingJQ= false;
+function bootStrap(){
+	
+	
+	if(loadingJQ && !window.jQuery)
+	{
+		console.log('delaying and waiting for jquery to get loaded');
+		setTimeout(bootStrap,500);
+		return;		
+	}
+	if(!window.jQuery)
+	{
+		console.log('loading jquery...');
+		loadingJQ=true;
+		(function(){
+		  var newscript = document.createElement('script');
+			 newscript.type = 'text/javascript';
+			 newscript.async = true;
+			 newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+		  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+		})();
+		setTimeout(bootStrap,500);
+		return;
+	}
+	console.log('jQuery is loaded');	
+	$(document).unbind('keydown').keydown(function(e){
 		console.log('keydown');
-		
-	}
-	if(e.keyCode==37)
-	{
-		next();
-	}
-	else if(e.keyCode==39)
-	{
-		prev();
-		
-	}
-});
-$('.prev_div').unbind('click').click(prev);
-$('.next_div').unbind('click').click(next);
-function prev(e){
-	$('#epaper_image').attr('src')
-	var fName=$('#epaper_image').attr('src');
-	var arrFName = fName.split('/');
-	var fParts=arrFName[arrFName.length-1].replace('.jpg','').split('-');
-	var nextFileNo=((~~(fParts[fParts.length-1]))-1);
-	console.log(fParts);
-	fParts[fParts.length-1]=nextFileNo.toString().padStart(3,'0');
-	console.log(fParts);
-	arrFName[arrFName.length-1]=fParts.join('-')+'.jpg';
-	fName=arrFName.join('/');
-	$('#epaper_image').attr('src',fName);
-	
-	
-	
-	if(!!e) e.preventDefault();
-	return false;
-	
-}
+		if(!!e.keyCode){
+			console.log('keydown');
+			
+		}
+		if(e.keyCode==37)
+		{
+			next();
+		}
+		else if(e.keyCode==39)
+		{
+			prev();
+			
+		}
+	});
 
-function next(e){
-		$('#epaper_image').attr('src')
-	var fName=$('#epaper_image').attr('src');
-	var arrFName = fName.split('/');
-	var fParts=arrFName[arrFName.length-1].replace('.jpg','').split('-');
-	var nextFileNo=((~~(fParts[fParts.length-1]))+1);
-	console.log(fParts);
-	fParts[fParts.length-1]=nextFileNo.toString().padStart(3,'0');
-	console.log(fParts);
-	arrFName[arrFName.length-1]=fParts.join('-')+'.jpg';
-	fName=arrFName.join('/');
-	$('#epaper_image').attr('src',fName);
-	if(!!e) e.preventDefault();
-	return false;
+	function prev(e){
+		//	$('#epaper_image').attr('src')
+		
+		var sel1=$('#epaper_image')[0].src.replace('large','small');
+		var sel=$(`.item a img[src$="${sel1}"]`);
+		var nxt= sel.closest('.owl-item').prev();
+		console.log($('.item a img',nxt)[0].src);
+		var imgT=$('.item a img',nxt)[0].src.replace('small','large');
+		$('#epaper_image')[0].src=imgT;
+		if(!!e) e.preventDefault();
+		return false;
+	}
+
+	function next(e){
+		
+			var sel1=$('#epaper_image')[0].src.replace('large','small');
+		var sel=$(`.item a img[src$="${sel1}"]`);
+		var nxt= sel.closest('.owl-item').next();
+		console.log($('.item a img',nxt)[0].src);
+		var imgT=$('.item a img',nxt)[0].src.replace('small','large');
+		$('#epaper_image')[0].src=imgT;
+		if(!!e) e.preventDefault();
+		return false;
+	}
+	$('.prev_div').unbind('click').click(prev);
+	$('.next_div').unbind('click').click(next);
 }
+bootStrap();
