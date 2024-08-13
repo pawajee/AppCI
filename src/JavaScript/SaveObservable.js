b10e59c5dc25e4ce6710688259513a42201f4d25
pawajee <pawajee@users.noreply.github.com>
2024-08-13 21:45:37 +0300

    Create SaveObservable.js

---
+++ src/JavaScript/SaveObservable.js
@@ -0,0 +1,43 @@
+import { Observable,fromEvent,from,of } from 'rxjs';
+import { tap, map,concatMap } from 'rxjs/operators';
+
+var saveBtn=document.querySelector('#saveBtn');
+fromEvent(saveBtn, 'click')
+  .pipe(
+    concatMap(click => save())
+      )
+  .subscribe(result => {
+    
+    console.log(`result:${typeof result}:${result}`);
+    // result is a stream!
+  });
+/*
+  var savePrm=new Promise(
+    (res, reject)=>{
+      //console.log("promise called");
+      setTimeout(()=>{
+        console.log("solving the promise");
+        res("save..");
+      }
+        ,3000
+      );
+  });
+ 
+ function save(){ 
+   console.log('inside save..');
+   return from(savePrm);
+ }
+  */
+   //from(savePrm);
+  
+function save(){
+  console.log(`inside save..`);
+  return new Observable<string>((observer:any)=> {
+    let count = 0;
+  
+    setTimeout(() => {
+      observer.next( "saved...");
+      observer.complete();
+    }, 1000);
+  });
+}
